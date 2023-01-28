346a4ff3ffed502279fc70af2f8aebf8db3a96bd
06ED <hsbest14@yandex.ru>
2023-01-28 15:47:02 +0300

    Created api helper tool

--- lib/lib.py
+++ lib/lib.py
@@ -2,7 +2,7 @@ import requests
 
 
 class Lib:
-    server_address = "http://127.0.0.1:5000"
+    server_address = "http://127.0.0.1:4000"
     session = requests.Session()
 
     @classmethod
@@ -11,17 +11,32 @@ class Lib:
             "name": name,
             "phone": phone,
             "password": password
+        }).json()
+        return res["id"]
+
+    @classmethod
+    def check_password(cls, phone, password):
+        res = cls.session.post(cls.server_address + "/api/user/check_pass", json={
+            "phone": phone,
+            "password": password
+        }).json()
+        return res == "correct"
+
+    @classmethod
+    def get_user(cls, user_id):
+        return cls.session.get(cls.server_address + "/api/user/get", json={
+            "id": user_id
+        }).json()
+
+    @classmethod
+    def get_last_recipe_request(cls, user_id):
+        return cls.session.get(cls.server_address + "/api/user/get_last_recipe", json={
+            "id": user_id
+        }).json()
+
+    @classmethod
+    def set_last_recipe_request(cls, user_id, recipe_id):
+        cls.session.get(cls.server_address + "/api/user/set_last_recipe", json={
+            "id": user_id,
+            "recipe_id": recipe_id
         })
-        return res.json()["id"]
-
-    @classmethod
-    def check_password(cls, user_id, password): ...
-
-    @classmethod
-    def get_user(cls, user_id): ...
-
-    @classmethod
-    def get_last_recipe_request(cls, user_id): ...
-
-    @classmethod
-    def set_last_recipe_request(cls, user_id, recipe_id): ...
