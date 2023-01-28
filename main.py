c0c2f1f55e434c8ac61d203413240354aab850dc
J1mmyChanga <epiakovmartynenkovv@yandex.ru>
2023-01-28 15:45:35 +0300

    ban

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ class Login(QtWidgets.QWidget, LoginWindow):
     def check_data(self):
         num, password = self.number_le.text(), self.password_le.text()
         ###проверка есть ли в бд###
+        open_settings()
 
     @staticmethod
     def open_reg():
@@ -63,58 +64,7 @@ class Settings(QtWidgets.QWidget, SettingsWindow):
         self.find_rec_pb.clicked.connect(self.check_data_filled_correctly)
 
     def check_data_filled_correctly(self):
-        global nickname
-        nicknames = [i[0] for i in self.con.cursor().execute("""SELECT nickname FROM users""").fetchall()]
-        emails = [i[0] for i in self.con.cursor().execute("""SELECT email FROM users""").fetchall()]
-        name = self.le_name.text()
-        nickname = self.le_nickname.text()
-        email = self.le_email.text()
-        password = self.le_password.text()
-
-        if not (name and nickname and email and password):
-            self.raise_exception('Необходимо заполнить все поля!')
-            return
-
-        if len(password) < 8 and password:
-            self.raise_exception('Пароль должен содержать как минимум 8 символов.')
-            return
-
-        if len(list(filter(lambda x: x in "0123456789", password))) < 2 and password:
-            self.raise_exception('Пароль должен содержать как минимум 2 цифры.')
-            return
-
-        if nickname in nicknames:
-            self.raise_exception('Пользователь с таким никнеймом уже существует!')
-            return
-
-        if email in emails:
-            self.raise_exception('Пользователь с таким адресом электронной почты уже существует!')
-            return
-
-        self.con.cursor().execute('''INSERT INTO users(name,nickname,email,password)
-                                            VALUES (?,?,?,?)''',
-                                  (name, nickname, email, password))
-        self.con.cursor().execute(f'''CREATE TABLE {nickname}_clothes (
-                                            id      INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                                            clothes INTEGER NOT NULL REFERENCES clothes (clothesid) UNIQUE);''')
-
-        self.con.cursor().execute(f'''CREATE TABLE {nickname}_favourite (
-                                            id     INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                                            look   INTEGER REFERENCES looks (looksid) UNIQUE NOT NULL,
-                                            output STRING  UNIQUE NOT NULL);''')
-
-        self.con.cursor().execute(f'''CREATE TABLE {nickname}_looks (
-                                            id     INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                                            look   INTEGER UNIQUE NOT NULL REFERENCES looks (looksid),
-                                            output STRING UNIQUE NOT NULL);''')
-        update_session(self.le_nickname.text(), self.le_password.text())
-        output_photo = self.convert_to_binary(self.def_picture)
-        self.con.cursor().execute('''UPDATE users
-                        SET image = ?
-                        WHERE nickname = ?''', (output_photo, nickname))
-        self.con.commit()
-
-        open_ward()
+        pass
 
 
 class YourRecipe(QtWidgets.QWidget, YourRecipeWindow):
